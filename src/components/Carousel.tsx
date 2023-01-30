63a91408e1ae27d2e447b5cc3ae4c055a2ddf943
milonte <milonte@gmail.com>
2023-01-30 12:16:48 +0100

    feat: display carrousel condition function

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -9,6 +9,7 @@ interface CarouselProps {
 class Carousel extends React.Component<CarouselProps, { index: number }> {
     constructor(props: any) {
         super(props);
+        this.displayButton = this.displayButton.bind(this);
         this.handlePrevIndex = this.handlePrevIndex.bind(this);
         this.handleNextIndex = this.handleNextIndex.bind(this);
         this.state = { index: 0 };
@@ -23,14 +24,22 @@ class Carousel extends React.Component<CarouselProps, { index: number }> {
         this.setState({ index: indexResult });
     }
 
-    render() {
-        return (
-            <div id="carousel">
-                <img className="picture" src={this.props.pictures[this.state.index]} alt={`${this.state.index}`} />
+    displayButton() {
+        if (this.props.pictures.length > 1) {
+            return (
                 <div className='buttons'>
                     <button className='prev-btn' onClick={this.handlePrevIndex}>&#x2039;</button>
                     <button className='next-btn' onClick={this.handleNextIndex}>&#x203A;</button>
                 </div>
+            )
+        } else { return (<div className='buttons'></div>) }
+    }
+
+    render() {
+        return (
+            <div id="carousel">
+                <img className="picture" src={this.props.pictures[this.state.index]} alt={`${this.state.index}`} />
+                <this.displayButton />
             </div>
         );
     }
