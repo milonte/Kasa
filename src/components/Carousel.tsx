f31bbc05a68e3fcbb7d7090eff311e5eb0d0eb76
milonte <milonte@gmail.com>
2023-02-05 09:48:15 +0100

    fix; refactore Carousel

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,48 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/components/carousel.scss';
 
 interface CarouselProps {
     pictures: Array<string>;
 }
 
+export default function Carousel(props: CarouselProps) {
+    const [index, setIndex] = useState(0);
 
-class Carousel extends React.Component<CarouselProps, { index: number }> {
-    constructor(props: any) {
-        super(props);
-        this.displayButton = this.displayButton.bind(this);
-        this.handlePrevIndex = this.handlePrevIndex.bind(this);
-        this.handleNextIndex = this.handleNextIndex.bind(this);
-        this.state = { index: 0 };
+    function handlePrevIndex() {
+        const indexResult = index > 0 ? index - 1 : props.pictures.length - 1;
+        setIndex(indexResult);
     }
 
-    handlePrevIndex() {
-        const indexResult = this.state.index > 0 ? this.state.index - 1 : this.props.pictures.length - 1;
-        this.setState({ index: indexResult });
-    }
-    handleNextIndex() {
-        const indexResult = this.state.index < this.props.pictures.length - 1 ? this.state.index + 1 : 0;
-        this.setState({ index: indexResult });
+    function handleNextIndex() {
+        const indexResult = index < props.pictures.length - 1 ? index + 1 : 0;
+        setIndex(indexResult);
     }
 
-    displayButton() {
-        if (this.props.pictures.length > 1) {
-            return (
-                <div className='buttons'>
-                    <button className='prev-btn' onClick={this.handlePrevIndex}>&#x2039;</button>
-                    <button className='next-btn' onClick={this.handleNextIndex}>&#x203A;</button>
-                </div>
-            )
-        } else { return (<div className='buttons'></div>) }
-    }
-
-    render() {
+    function displayButton() {
         return (
-            <div id="carousel">
-                <img className="picture" src={this.props.pictures[this.state.index]} alt={`${this.state.index}`} />
-                <this.displayButton />
+            <div className='buttons'>
+                {props.pictures.length > 1 ?
+                    <>
+                        <button className='prev-btn' onClick={handlePrevIndex}>&#x2039;</button>
+                        <button className='next-btn' onClick={handleNextIndex}>&#x203A;</button>
+                    </>
+                    : <></>}
+
             </div>
-        );
+        )
     }
+
+    return (
+        <div id="carousel">
+            <img className="picture" src={props.pictures[index]} alt={`${index}`} />
+
+            {displayButton()}
+        </div>
+    );
 
 }
-export default Carousel
