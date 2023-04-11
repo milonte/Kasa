585a5e2abdd89d0d061751276234e2b22a13858c
milonte <milonte@gmail.com>
2023-04-11 18:47:54 +0200

    fix: remove useless dependencies + add carousel index display

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -18,25 +18,20 @@ export default function Carousel(props: CarouselProps): ReactElement {
         setIndex(indexResult);
     }
 
-    function displayButton(): ReactElement {
-        return (
-            <div className='buttons'>
-                {props.pictures.length > 1 ?
-                    <>
-                        <button className='prev-btn' onClick={handlePrevIndex}>&#x2039;</button>
-                        <button className='next-btn' onClick={handleNextIndex}>&#x203A;</button>
-                    </>
-                    : <></>}
-
-            </div>
-        )
-    }
-
     return (
         <div id="carousel">
             <img className="picture" src={props.pictures[index]} alt={`${index}`} />
 
-            {displayButton()}
+            {props.pictures.length > 1 ?
+                <>
+                    <div className='buttons'>
+
+                        <button className='prev-btn' onClick={handlePrevIndex}>&#x2039;</button>
+                        <button className='next-btn' onClick={handleNextIndex}>&#x203A;</button>
+                    </div>
+                    <div className='current-index'>{index + 1}/{props.pictures.length}</div>
+                </>
+                : <></>}
         </div>
     );
 
